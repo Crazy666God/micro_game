b00198f5c345aa0b72c041b09e01584c0c1fc7ba
Crazy666God <posokhov.ivan@yandex.ru>
2022-08-30 21:25:48 +0300

    added game_scene.dart

---
+++ lib/scenes/game_scene.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:micro_game/entities/bullet.dart';
+import 'package:micro_game/entities/player.dart';
+import 'package:micro_game/scenes/app_scene.dart';
+import 'package:micro_game/utilits/global_vars.dart';
+
+class GameScene extends AppScene {
+  Player _player = Player();
+  double _startGlobalPosition = 0;
+  List<Bullet> _listBullets = [];
+  List<Widget> _listWidgets = [];
+
+  @override
+  Widget buildScene() {
+    return Stack(
+      children: [
+        _player.build(),
+        Positioned(
+          top: 0,
+          left: 0,
+          child: Container(
+            width: GlobalVars.screenWidth / 2,
+            height: GlobalVars.screenHeiht,
+            child: GestureDetector(
+              onPanStart: _onPanStart,
+              onPanUpdate: _onPanUpdate,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: GlobalVars.screenWidth / 2,
+          child: Container(
+            width: GlobalVars.screenWidth / 2,
+            height: GlobalVars.screenHeiht / 2,
+            child: GestureDetector(
+              onTap: _onAcceleration,
+            ),
+          ),
+        ),
+        Positioned(
+          top: GlobalVars.screenHeiht / 2,
+          left: GlobalVars.screenWidth / 2,
+          child: Container(
+            width: GlobalVars.screenWidth / 2,
+            height: GlobalVars.screenHeiht / 2,
+            child: GestureDetector(
+              onTap: _onShoot,
+            ),
+          ),
+        ),
+        Stack(children: _listWidgets,)
+      ],
+    );
+  }
+
+  @override
+  void update() {
+    _player.update();
+    _listWidgets.clear(); // or _listWidgets = [];
+    _listBullets.removeWhere((element) => !element.visible);
+    _listBullets.forEach((element) { 
+      _listWidgets.add(element.build());
+      element.update();
+    });
+  }
+
+  void _onPanStart(DragStartDetails details) {
+    _startGlobalPosition = details.globalPosition.dx;
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    double updateGlobalPosition = details.globalPosition.dx;
+    if (updateGlobalPosition > _startGlobalPosition + 30) {
+      //right
+      _player.isMoveRight = true;
+    }
+    if (updateGlobalPosition < _startGlobalPosition - 30) {
+      //left
+      _player.isMoveLeft = true;
+    }
+  }
+
+  void _onAcceleration() {
+    _player.isAcceleration = !_player.isAcceleration;
+  }
+
+  void _onShoot() {
+    _listBullets.add(Bullet(playerAngle: _player.getAbgle, x: _player.x, y: _player.y));
+  }
+}
