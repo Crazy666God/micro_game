330aae0e6851758127cc6caca5736b28e5922b58
Crazy666God <posokhov.ivan@yandex.ru>
2022-08-30 21:16:43 +0300

    renamed the file

---
+++ lib/utilits/global_vars.dart
@@ -0,0 +1,8 @@
+import 'package:micro_game/scenes/app_scene.dart';
+import 'package:micro_game/scenes/game_scene.dart';
+
+class GlobalVars {
+  static AppScene currentScene = GameScene();
+  static double screenWidth = 0;
+  static double screenHeiht = 0;
+}
