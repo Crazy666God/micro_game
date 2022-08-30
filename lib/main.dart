4e789f65e426a839c1a1601c3780597aba6c1a0e
Crazy666God <posokhov.ivan@yandex.ru>
2022-08-30 13:42:01 +0300

    added orientations and run game

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:micro_game/game.dart';
+import 'package:micro_game/main_loop.dart';
 
 void main() {
-  
-  
-  runApp();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(
+    () {
+      SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom],
+      );
+
+      runApp(
+        const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Game(),
+        ),
+      );
+    },
+  );
 }
