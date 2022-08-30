58f3e9866e1f9caf4188a738d3db4f91442672a9
Crazy666God <posokhov.ivan@yandex.ru>
2022-08-30 13:40:46 +0300

    created game.dart

---
+++ lib/game.dart
@@ -0,0 +1,47 @@
+import 'dart:isolate';
+
+import 'package:flutter/material.dart';
+import 'package:micro_game/main_loop.dart';
+import 'package:micro_game/utilits/common_vars.dart';
+
+import 'entities/player.dart';
+
+class Game extends StatefulWidget {
+  const Game({super.key});
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  late ReceivePort _receivePort;
+  late Isolate _isolateLoop;
+
+  late Player player;
+
+  void startIsolateLoop() async {
+    _receivePort = ReceivePort();
+    _isolateLoop = await Isolate.spawn(mainLoop, _receivePort.sendPort);
+    _receivePort.listen((message) { 
+      setState(() {});
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(isFirstStartGame) {
+      startIsolateLoop();
+      isFirstStartGame = false;
+      player = Player();
+    }
+
+    player.update();
+    
+    return Stack(
+      children: [
+        player.build(),
+      ],
+    );
+  }
+}
