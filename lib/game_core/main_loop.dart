9adb623f5f60f38d11ed3302b2224ae69a578fa7
Crazy666God <posokhov.ivan@yandex.ru>
2022-08-31 11:27:25 +0300

    variable removed _updates

--- lib/game_core/main_loop.dart
+++ lib/game_core/main_loop.dart
@@ -3,30 +3,23 @@ import 'dart:isolate';
 bool _running = true;
 
 void mainLoop(SendPort sendPort) async {
-  final int _fps = 60;
-  final int _second = 1000;
-  final double _updateTime = _second / _fps;
-  double _updates = 0;
+  const int fps = 50;
+  const int second = 1000;
+  const double updateTime = second / fps;
 
-  Stopwatch _loopWatch = Stopwatch()..start();
-  
-  Stopwatch _timerWatch = Stopwatch()..start();
+  Stopwatch loopWatch = Stopwatch()..start();
+  Stopwatch timerWatch = Stopwatch()..start();
 
-  while(_running) {
-    if(_loopWatch.elapsedMilliseconds >= _updateTime) {
-      ++_updates;
-      _loopWatch.reset();
+  while (_running) {
+    if (loopWatch.elapsedMilliseconds >= updateTime) {
+      loopWatch.reset();
       sendPort.send(true);
     }
- 
-    if(_timerWatch.elapsedMilliseconds > _second) {
-      print('${DateTime.now()} FPS: $_updates');
-      _updates = 0;
-      _timerWatch.reset();
+
+    if (timerWatch.elapsedMilliseconds > second) {
+      timerWatch.reset();
     }
-
   }
-
 }
 
 void stopLoop() {
