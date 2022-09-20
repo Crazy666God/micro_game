0afd4bf127e7e6cbd3db29b694f40791e0059b7b
Crazy666God <posokhov.ivan@yandex.ru>
2022-09-20 20:47:49 +0300

    added entity meteor

---
+++ lib/entities/meteor.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:micro_game/entities/entity.dart';
+import 'package:micro_game/utilits/global_vars.dart';
+
+class Meteor extends Entity {
+  final double _speed = 3;
+  final int angle;
+  double _rotationAngle = 0;
+
+  Meteor({
+    required this.angle, 
+    required super.coordinates,
+    required super.spriteName,
+    required super.spriteNumber,
+    required super.spriteSize,
+  });
+
+  @override
+  Widget build() {
+    return Positioned(
+      top: coordinates.y,
+      left: coordinates.x,
+      child: FractionalTranslation(
+        translation: const Offset(-0.5, -0.5),
+        child: SizedBox(
+          width: spriteSize.width,
+          height: spriteSize.height,
+          child: visible
+              ? Transform.rotate(
+                  alignment: Alignment.center,
+                  angle: _rotationAngle,
+                  child: sprite,
+                )
+              : const SizedBox(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void move() {
+    _rotationAngle += 0.025;
+    coordinates.x += sin(angle * 0.0175) * _speed;
+    coordinates.y -= cos(angle * 0.0175) * _speed;
+    if (coordinates.x > GlobalVars.screenWidth + 200 ||
+        coordinates.y > GlobalVars.screenHeiht + 200 ||
+        coordinates.x < -200 ||
+        coordinates.y < -200) {
+      visible = false;
+    }
+  }
+}
